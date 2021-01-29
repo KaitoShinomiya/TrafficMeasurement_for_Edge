0914eab331569b4cf1e3a42ce087aee8eaf07be3
H. Kanazawa <43166752+urban-side@users.noreply.github.com>
2021-01-29 18:07:11 +0900

    setting condition 4

--- train_ssd.py
+++ train_ssd.py
@@ -308,7 +308,7 @@ if __name__ == '__main__':
                 if net.base_net[num_block][num_layer]._get_name() is not 'ReLU6':
                     m = prune.l1_unstructured(net.base_net[num_block][num_layer],
                                               name='weight',
-                                              amount=0.25)
+                                              amount=0.5)
                     net.base_net[num_block][num_layer] = prune.remove(m, name='weight')
         else:
             for num_layer in range(len(net.base_net[num_block].conv)):
@@ -317,7 +317,7 @@ if __name__ == '__main__':
                 if net.base_net[num_block].conv[num_layer]._get_name() is not 'ReLU6':
                     m = prune.l1_unstructured(net.base_net[num_block].conv[num_layer],
                                               name='weight',
-                                              amount=0.25)
+                                              amount=0.5)
                     net.base_net[num_block].conv[num_layer] = prune.remove(m, name='weight')
 
     pruned_net = net
@@ -351,7 +351,7 @@ if __name__ == '__main__':
     model_loss = []
     train_time = []
     val_time = []
-    condition = 'cond_2'
+    condition = 'cond_4'
     for epoch in range(last_epoch + 1, args.num_epochs):
         scheduler.step()
 
