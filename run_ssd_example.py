f96e360c88ba3d39f432a7a9dd900ed6e35d53cc
H. Kanazawa <43166752+urban-side@users.noreply.github.com>
2021-01-29 17:38:29 +0900

    init

---
+++ run_ssd_example.py
@@ -0,0 +1,102 @@
+from vision.ssd.vgg_ssd import create_vgg_ssd, create_vgg_ssd_predictor
+from vision.ssd.mobilenetv1_ssd import create_mobilenetv1_ssd, create_mobilenetv1_ssd_predictor
+from vision.ssd.mobilenetv1_ssd_lite import create_mobilenetv1_ssd_lite, create_mobilenetv1_ssd_lite_predictor
+from vision.ssd.squeezenet_ssd_lite import create_squeezenet_ssd_lite, create_squeezenet_ssd_lite_predictor
+from vision.ssd.mobilenet_v2_ssd_lite import create_mobilenetv2_ssd_lite, create_mobilenetv2_ssd_lite_predictor
+from vision.utils.misc import Timer
+import cv2
+import sys
+import os
+from tqdm import tqdm
+
+
+if len(sys.argv) < 5:
+    print('Usage: python run_ssd_example.py <net type> <model path> <label path> <image path>')
+    sys.exit(0)
+net_type = sys.argv[1]
+model_path = sys.argv[2]
+label_path = sys.argv[3]
+image_path = sys.argv[4]
+image_dir = "./sample_data/"
+image_path_list = os.listdir(image_dir)  # [example.jpg, example1.png, ..., exampleXX.jpg]
+
+class_names = [name.strip() for name in open(label_path).readlines()]
+
+if net_type == 'vgg16-ssd':
+    net = create_vgg_ssd(len(class_names), is_test=True)
+elif net_type == 'mb1-ssd':
+    net = create_mobilenetv1_ssd(len(class_names), is_test=True)
+elif net_type == 'mb1-ssd-lite':
+    net = create_mobilenetv1_ssd_lite(len(class_names), is_test=True)
+elif net_type == 'mb2-ssd-lite':
+    net = create_mobilenetv2_ssd_lite(len(class_names), is_test=True)
+elif net_type == 'sq-ssd-lite':
+    net = create_squeezenet_ssd_lite(len(class_names), is_test=True)
+else:
+    print("The net type is wrong. It should be one of vgg16-ssd, mb1-ssd and mb1-ssd-lite.")
+    sys.exit(1)
+net.load(model_path)
+
+if net_type == 'vgg16-ssd':
+    predictor = create_vgg_ssd_predictor(net, candidate_size=200)
+elif net_type == 'mb1-ssd':
+    predictor = create_mobilenetv1_ssd_predictor(net, candidate_size=200)
+elif net_type == 'mb1-ssd-lite':
+    predictor = create_mobilenetv1_ssd_lite_predictor(net, candidate_size=200)
+elif net_type == 'mb2-ssd-lite':
+    predictor = create_mobilenetv2_ssd_lite_predictor(net, candidate_size=200)
+elif net_type == 'sq-ssd-lite':
+    predictor = create_squeezenet_ssd_lite_predictor(net, candidate_size=200)
+else:
+    predictor = create_vgg_ssd_predictor(net, candidate_size=200)
+
+"""
+box = boxes[i, :]
+orig_image = cv2.imread(image_path)
+image = cv2.cvtColor(orig_image, cv2.COLOR_BGR2RGB)
+boxes, labels, probs = predictor.predict(image, 10, 0.4)
+
+for i in range(boxes.size(0)):
+    cv2.rectangle(orig_image, (box[0], box[1]), (box[2], box[3]), (255, 255, 0), 4)
+    label = f"{class_names[labels[i]]}: {probs[i]:.2f}"
+    cv2.putText(orig_image, label,
+                (box[0] + 20, box[1] + 40),
+                cv2.FONT_HERSHEY_SIMPLEX,
+                1,  # font scale
+                (255, 0, 255),
+                2)  # line type
+                
+path = "run_ssd_example_output.jpg"
+cv2.imwrite(path, orig_image)
+print(f"Found {len(probs)} objects. The output image is {path}")
+"""
+
+for image_path in tqdm(image_path_list):
+    orig_image = cv2.imread(image_dir + image_path)
+    image = cv2.cvtColor(orig_image, cv2.COLOR_BGR2RGB)
+    boxes, labels, probs = predictor.predict(image, 10, 0.4)
+
+    for box, label in zip(boxes, labels):
+        out_file = os.path.splitext(image_path)[0] + ".csv"
+        anno_dir = "../well-D_data_20200901/workspace_20200902/output_anno/"
+        write_contents = "{},{},{},{},{}\n".format(box[0],box[1],box[2],box[3],class_names[label])    # (xmin, ymin, xmax, ymax, car)
+        with open(anno_dir + out_file, mode="w") as f:
+            f.writelines(write_contents)
+
+    """
+        for i in range(boxes.size(0)):
+        box = boxes[i, :]
+        cv2.rectangle(orig_image, (box[0], box[1]), (box[2], box[3]), (255, 255, 0), 4)
+        label = f"{class_names[labels[i]]}: {probs[i]:.2f}"
+        cv2.putText(orig_image, label,
+                    (box[0] + 20, box[1] + 40),
+                    cv2.FONT_HERSHEY_SIMPLEX,
+                    1,  # font scale
+                    (255, 0, 255),
+                    2)  # line type
+                    
+    path = "_output.jpg"
+    print(image_path+path)
+    cv2.imwrite(image_path+path, orig_image)
+    print(f"Found {len(probs)} objects. The output image is {path}")
+    """
